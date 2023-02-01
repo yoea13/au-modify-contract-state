3a6e9a2449824ef7239c957224f65b59a9715f08
yoea13 <yoea13@gmail.com>
2023-02-01 15:23:15 -0300

    inicio

---
+++ hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
